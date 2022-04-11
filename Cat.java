46bf086d0772f05f7757988a4be4824cef4c0ed3
Mariusz PS <103026332+MariuszPS@users.noreply.github.com>
2022-04-11 14:48:04 +0200

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,32 @@
+package com.cc.java;
+
+public class Cat {
+    
+    private String name;
+    private String furColor;
+    private int age;
+
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+    public String getStringAttributes(String flag) {
+        switch (flag) {
+            case "#name":
+                return name;
+            case "#color":
+                return furColor;
+            default:
+                return "ERROR: ";
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+   
+
+}
